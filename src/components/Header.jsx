4a3146f6c8814aa48dd632d0eeafc30b3240b3eb
Abdul Muksith Rizvi <57699415+abdulmuksith3@users.noreply.github.com>
2025-04-01 18:58:53 +0300

    FE complete

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,7 +2,10 @@ import React from 'react';
 
 const Header = ({ onCompose }) => (
   <header className="flex justify-between items-center">
-    <h1 className="text-2xl font-bold">Spam Classifier Mail</h1>
+    <div>
+        <h1 className="text-2xl font-bold">Email Threat Detection Demo</h1>
+        <h2 className="text font-bold">AICC5201 - Machine & Deep Learning</h2>
+    </div>
     <button
       onClick={onCompose}
       className="bg-blue-600 text-white px-4 py-2 rounded-xl hover:bg-blue-700"
--- src/components/InboxList.jsx
+++ src/components/InboxList.jsx
@@ -3,22 +3,28 @@ import React from 'react';
 const InboxList = ({ emails, onSelect }) => (
   <div className="bg-white rounded-xl shadow-md p-4 h-full">
     <h2 className="text-xl font-semibold mb-2">Inbox</h2>
-    <ul>
-      {emails.map((email, idx) => (
-        <li
-          key={idx}
-          onClick={() => onSelect(email)}
-          className="p-2 border-b hover:bg-gray-100 cursor-pointer"
-        >
-          <div className="flex justify-between items-center">
-            <span className="font-medium">{email.subject}</span>
-            <span className={`text-xs px-2 py-1 rounded ${email.isSpam ? 'bg-red-100 text-red-600' : 'bg-green-100 text-green-700'}`}>
-              {email.isSpam ? 'Spam' : 'Not Spam'}
-            </span>
-          </div>
-        </li>
-      ))}
+    {emails.length > 0 
+    ? <ul>
+        {emails.map((email, idx) => (
+            <li
+            key={idx}
+            onClick={() => onSelect(email)}
+            className="p-2 border-b hover:bg-gray-100 cursor-pointer"
+            >
+            <div className="flex justify-between items-center">
+                <span className="font-medium">{email.subject}</span>
+                <span className={`text-xs px-2 py-1 rounded ${email.isSpam ? 'bg-red-100 text-red-600' : 'bg-green-100 text-green-700'}`}>
+                {email.isSpam ? 'Spam' : 'Not Spam'}
+                </span>
+            </div>
+            </li>
+        ))}
     </ul>
+    : <div>
+        Inbox is empty
+    </div>
+    }
+    
   </div>
 );
 
